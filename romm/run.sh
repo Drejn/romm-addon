9eba82e673ec066808a331195e4b2d5f922e17f1
Drejn <explorer.p@libero.it>
2026-05-14 11:06:04 +0200

    Update run.sh

--- romm/run.sh
+++ romm/run.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-BASE_PATH="{{ base_path }}"  # viene sostituito da Home Assistant
+BASE_PATH="{{ base_path }}"
 
 export ROMM_LIBRARY_PATH="$BASE_PATH/library"
 export ROMM_ROMS_PATH="$BASE_PATH/roms"
 export ROMM_MEDIA_PATH="$BASE_PATH/media"
 export ROMM_CONFIG_PATH="$BASE_PATH/config"
 
-# eventuali altre env che già stai passando (DB, ecc.)
-
-# porta su cui RomM ascolta
-export PORT=8998
 export HOST=0.0.0.0
+export PORT=8998
 
-# avvia RomM (come fa l'immagine ufficiale)
 python -m romm
