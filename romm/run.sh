16c5bd2a7c1310fea1699ecffc4b00bab0168088
Drejn <explorer.p@libero.it>
2026-05-14 11:42:01 +0200

    Update run.sh

--- romm/run.sh
+++ romm/run.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
 set -e
-
-# Default base path
 BASE_PATH="/share/romm"
-
-# If Supervisor options exist, read base_path from /data/options.json
 if [ -f /data/options.json ]; then
   BASE_PATH=$(python - <<'PY'
-import json,sys
+import json
 try:
-    o=json.load(open('/data/options.json'))
-    print(o.get('base_path','/share/romm'))
-except Exception:
-    print('/share/romm')
+  o=json.load(open('/data/options.json'))
+  print(o.get('base_path','/share/romm'))
+except:
+  print('/share/romm')
 PY
 )
 fi
-
 export ROMM_LIBRARY_PATH="$BASE_PATH/library"
 export ROMM_ROMS_PATH="$BASE_PATH/roms"
 export ROMM_MEDIA_PATH="$BASE_PATH/media"
 export ROMM_CONFIG_PATH="$BASE_PATH/config"
-
 export HOST=0.0.0.0
 export PORT=8998
-
 exec python -m romm
